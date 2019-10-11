a188ef78fc10048d094b3bb4b50c320dcd0c4e64
Johan Bos <johan.bos@viaselect.com>
2019-10-11 14:11:25 +0200

    Fix  #11 Allow multiple players

--- lib/poker.ex
+++ lib/poker.ex
@@ -7,34 +7,36 @@ defmodule Poker do
 
   @doc """
   Determs a winner.
+  iex> Poker.winner([%Poker{name: "Black", cards: ["2H","2D","5S","9C","KD"]}, %Poker{name: "White", cards: ["3H","3D","5S","9C","QD"]}])
+  "White wins - pair: 3"
   """
-  @spec winner(%Poker{}, %Poker{}) :: String.t
-  #todo: @spec winner([%Poker{}]) :: String.t
-  def winner(a, b) do
-    a_hand = highest_hand(a)
-    b_hand = highest_hand(b)
-    cond do
-        # wins by hand
-        elem(a_hand, 0) > elem(b_hand,0) -> "#{a.name} wins - #{elem(a_hand, 2)}"
-        elem(a_hand, 0) < elem(b_hand,0) -> "#{b.name} wins - #{elem(b_hand, 2)}"
-        # wins by value with same hand
-        elem(a_hand, 1) > elem(b_hand,1) -> "#{a.name} wins - #{elem(a_hand, 2)}"
-        elem(a_hand, 1) < elem(b_hand,1) -> "#{b.name} wins - #{elem(b_hand, 2)}"
-        true -> "Tie"
+  @spec winner([%Poker{}]) :: String.t
+  def winner(hands) do
+    # analyze hands
+    highest_hands = Enum.map(hands, &highest_hand/1)
+    # sort by highest and then card values
+    |> Enum.sort_by(fn e -> (elem(e, 0) * -1000) - (elem(e, 1))  end)
+    |> Enum.take(2)
+    # if head equals second its a Tie
+    [first | [second | _]] = highest_hands
+    if (elem(first, 0) == elem(second, 0) && elem(first, 1) == elem(second, 1)) do
+        "Tie"
+    else
+        elem(first, 2)
     end
   end
 
   @doc """
   Determ the best hand by poker rules
   ## Examples
-    iex>Poker.highest_hand(%Poker{cards: ["2H","3D","5S","9C","KD"]})
-    {0, 12, "high card: king"}
-    iex>Poker.highest_hand(%Poker{cards: ["JH","3D","5S","JC","KD"]})
-    {1, 10, "pair: jack"}
-    iex>Poker.highest_hand(%Poker{cards: ["JH","3D","3S","JC","KD"]})
-    {2, 10, "two pair: jack"}
-    iex>Poker.highest_hand(%Poker{cards: ["AH","QD","QS","QC","KD"]})
-    {3, 11, "three of a kind: queen"}
+    iex>Poker.highest_hand(%Poker{name: "x", cards: ["2H","3D","5S","9C","KD"]})
+    {0, 12, "x wins - high card: king"}
+    iex>Poker.highest_hand(%Poker{name: "x", cards: ["JH","3D","5S","JC","KD"]})
+    {1, 10, "x wins - pair: jack"}
+    iex>Poker.highest_hand(%Poker{name: "x", cards: ["JH","3D","3S","JC","KD"]})
+    {2, 10, "x wins - two pair: jack"}
+    iex>Poker.highest_hand(%Poker{name: "x", cards: ["AH","QD","QS","QC","KD"]})
+    {3, 11, "x wins - three of a kind: queen"}
   """
   @spec highest_hand(%Poker{}) :: {integer, integer, String.t}
   def highest_hand(a) do
@@ -42,10 +44,10 @@ defmodule Poker do
     cond do
       #h = full house
       #h = flush
-      h = Hand.three_of_a_kind(analyzed_cards) -> {3, h.value, "three of a kind: " <> Card.name(hd(h.cards))}
-      h = Hand.two_pair(analyzed_cards) -> {2, h.value, "two pair: " <> Card.name(hd(h.cards))}
-      h = Hand.pair(analyzed_cards) -> {1, h.value, "pair: " <> Card.name(hd(h.cards)) }
-      h = Hand.high_card(analyzed_cards) -> {0, h.value, "high card: " <> Card.name(hd(h.cards))}
+      h = Hand.three_of_a_kind(analyzed_cards) -> {3, h.value, "#{a.name} wins - three of a kind: " <> Card.name(hd(h.cards))}
+      h = Hand.two_pair(analyzed_cards) -> {2, h.value, "#{a.name} wins - two pair: " <> Card.name(hd(h.cards))}
+      h = Hand.pair(analyzed_cards) -> {1, h.value, "#{a.name} wins - pair: " <> Card.name(hd(h.cards)) }
+      h = Hand.high_card(analyzed_cards) -> {0, h.value, "#{a.name} wins - high card: " <> Card.name(hd(h.cards))}
     end
   end
 end
--- test/poker_test.exs
+++ test/poker_test.exs
@@ -7,29 +7,31 @@ defmodule PokerTest do
   test "Poker winner returns Ace" do
     black = %Poker{name: "Black", cards: ["2H","3D","5S","9C","KD"]}
     white = %Poker{name: "White", cards: ["2C","3H","4S","8C","AH"]}
-    assert Poker.winner(black, white) == "White wins - high card: ace"
+    assert Poker.winner([black, white]) == "White wins - high card: ace"
   end
 
   test "Poker winner returns pair Ace" do
     black = %Poker{name: "Black", cards: ["2H","3D","5S","9C","KD"]}
     white = %Poker{name: "White", cards: ["2C","3H","4S","AC","AH"]}
-    assert Poker.winner(black, white) == "White wins - pair: ace"
+    assert Poker.winner([black, white]) == "White wins - pair: ace"
   end
 
-#todo:
+#todo: flush
 #Input: Black: 2H 4S 4C 3D 4H White: 2S 8S AS QS 3S
 #Output: White wins - flush
+
+#todo: have same pair, win on high card
 
   test "Poker winner returns 9" do
     black = %Poker{name: "Black", cards: ["2H","3D","5S","9C","6D"]}
     white = %Poker{name: "White", cards: ["2C","3H","4S","8C","6H"]}
-    assert Poker.winner(black, white) == "Black wins - high card: 9"
+    assert Poker.winner([black, white]) == "Black wins - high card: 9"
   end
 
   test "Poker winner returns Tie" do
     black = %Poker{name: "Black", cards: ["2H","3D","5S","9C","KD"]}
     white = %Poker{name: "White", cards: ["2D","3H","5C","9S","KH"]}
-    assert Poker.winner(black, white) == "Tie"
+    assert Poker.winner([black, white]) == "Tie"
   end
 
 end
